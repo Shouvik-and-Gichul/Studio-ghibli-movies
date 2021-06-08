53d495ed193d123957c5948129c6bdfd818057e7
gichul <gichul0317@gmail.com>
2021-06-07 22:38:53 -0400

    added spirited-away page and fixed repeating codes in the script.js

--- js/script.js
+++ js/script.js
@@ -9,45 +9,82 @@ app.getContents = function () {
       return response.json();
     })
     .then(function (apiData) {
-      console.log(apiData);
-      // response.forEach(function (item) {
-      //   console.log(item.title);
-      // });
       app.displayContents(apiData);
     });
 };
 
 app.displayContents = function (data) {
-  const body = document.querySelector("body");
+  const body = document.querySelector('body');
   const apiFetch = document.querySelector('.api-fetch');
-  const h3Element = document.createElement('h3');
-  const h2Element = document.createElement('h2');
-  const pElementOne = document.createElement('p');
-  const pElementTwo = document.createElement('p');
-
-
-  for (let i = 0; i < data.length; i++) {
-    // console.log(data[i].title);
-    if (body.className === "castle-in-the-sky") {
-      h3Element.textContent = `Directed by ${data[0].director}`;
-      h2Element.textContent = `${data[0].title} ${data[0].original_title}`;
-      pElementOne.textContent = data[0].description;
-      pElementTwo.textContent = `Year of Production: ${data[0].release_date}, Runtime: ${data[0].running_time}, Rating: ${data[0].rt_score}`;
-      apiFetch.appendChild(h3Element);
-      apiFetch.appendChild(h2Element);
-      apiFetch.appendChild(pElementOne);
-      apiFetch.appendChild(pElementTwo);
-    } else if (body.className === "grave-of-the-fireflies") {
-      h3Element.textContent = `Directed by ${data[1].director}`;
-      h2Element.textContent = `${data[1].title} ${data[1].original_title}`;
-      pElementOne.textContent = data[1].description;
-      pElementTwo.textContent = `Year of Production: ${data[1].release_date}, Runtime: ${data[1].running_time} Rating: ${data[1].rt_score}`;
-      apiFetch.appendChild(h3Element);
-      apiFetch.appendChild(h2Element);
-      apiFetch.appendChild(pElementOne);
-      apiFetch.appendChild(pElementTwo);
+  data.forEach(function (apiData, index) {
+    const h3Element = document.createElement('h3');
+    const h2Element = document.createElement('h2');
+    const pElementOne = document.createElement('p');
+    const pElementTwo = document.createElement('p');
+    const {
+      director,
+      title,
+      original_title,
+      description,
+      release_date,
+      running_time,
+      rt_score,
+    } = apiData;
+    if (body.className === 'castle-in-the-sky' && index === 0) {
+      app.makeAndAppendElement(
+        h3Element,
+        h2Element,
+        pElementOne,
+        pElementTwo,
+        apiFetch,
+        apiData
+      );
+    } else if (body.className === 'grave-of-the-fireflies' && index === 1) {
+      app.makeAndAppendElement(
+        h3Element,
+        h2Element,
+        pElementOne,
+        pElementTwo,
+        apiFetch,
+        apiData
+      );
+    } else if (body.className === 'spirited-away' && index === 10) {
+      app.makeAndAppendElement(
+        h3Element,
+        h2Element,
+        pElementOne,
+        pElementTwo,
+        apiFetch,
+        apiData
+      );
     }
+  });
+};
+
+app.getApiData = function (data) {
+  for (let i = 0; i < data.length; i++) {
+    const {
+      director,
+      title,
+      original_title,
+      description,
+      release_date,
+      running_time,
+      rt_score,
+    } = data;
   }
+  return data;
+};
+
+app.makeAndAppendElement = function (h3, h2, p1, p2, apiFetch, getApiData) {
+  h3.textContent = `Directed by ${getApiData.director}`;
+  h2.textContent = `${getApiData.title} ${getApiData.original_title}`;
+  p1.textContent = `${getApiData.description}`;
+  p2.textContent = `Year of Production: ${getApiData.release_date}, Runtime: ${getApiData.running_time}, Rating: ${getApiData.rt_score}`;
+  apiFetch.appendChild(h3);
+  apiFetch.appendChild(h2);
+  apiFetch.appendChild(p1);
+  apiFetch.appendChild(p2);
 };
 
 app.displayTrailer = function () {
@@ -61,7 +98,6 @@ app.closeTrailer = function () {
   const closeBtn = document.querySelector('.close');
   closeBtn.addEventListener('click', function () {
     document.querySelector('.trailer-box').classList.toggle('hidden');
-    
     const video = document.querySelector('iframe');
     let source = video.src;
     video.src = '';
